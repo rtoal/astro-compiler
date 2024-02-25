431b6a5d08d4d0a20f0ea5608bc870bf29a26def
Ray Toal <rtoal@lmu.edu>
2024-02-24 17:01:50 -0800

    Checks in analyzer, not context

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,11 +1,14 @@
 import * as core from "./core.js"
 
-// Throw an error message that takes advantage of Ohm's messaging.
-// If you supply an Ohm tree node as the second parameter, this will
-// use Ohm's cool reporting mechanism.
-function error(message, node) {
-  const prefix = `${node?.source.getLineAndColumnMessage()}`
-  throw new Error(`${prefix}${message}`)
+// The single gate for error checking. Pass in a condition that must be true.
+// Use errorLocation to give contextual information about the error that will
+// appear: this should be an object whose "at" property is a parse tree node.
+// Ohm's getLineAndColumnMessage will be used to prefix the error message.
+function must(condition, message, errorLocation) {
+  if (!condition) {
+    const prefix = errorLocation.at.source.getLineAndColumnMessage()
+    throw new Error(`${prefix}${message}`)
+  }
 }
 
 class Context {
@@ -16,36 +19,11 @@ class Context {
     // nesting of scopes.
     this.locals = new Map()
   }
-  check(condition, message, { at }) {
-    if (!condition) error(message, at)
-  }
   add(name, entity) {
     this.locals.set(name, entity)
   }
-  lookup(id, { expecting } = {}) {
-    // Adds an entity to the context. If no expected kind is not passed in,
-    // then this method will return whatever it finds without further checking,
-    // or it will return undefined if no entity with the id's name is found.
-    // Otherwise the method will error if nothing was found or if the entity
-    // was not of the expected kind (Variable, Function, Procedure).
-    const entity = this.locals.get(id.sourceString)
-    if (!expecting) return entity
-    this.check(entity, `${id.sourceString} not defined`, { at: id })
-    this.checkKind(entity, expecting, { at: id })
-    return entity
-  }
-  checkKind(entity, kind, { at }) {
-    this.check(entity?.kind === kind, `${kind} expected`, { at })
-  }
-  checkIsWritable(variable, { at }) {
-    this.check(variable.writable, `${variable.name} is not writable`, { at })
-  }
-  checkArguments(callee, args, { at }) {
-    this.check(
-      args.length === callee.paramCount,
-      `Expected ${callee.paramCount} arg(s), found ${args.length}`,
-      { at }
-    )
+  lookup(id) {
+    return this.locals.get(id.sourceString)
   }
 }
 
@@ -53,6 +31,34 @@ export default function analyze(match) {
   // Astro is so trivial that the only required contextual information is
   // to keep track of the identifiers that have been declared.
   const context = new Context()
+
+  function mustHaveBeenFound(entity, name, at) {
+    must(entity, `Identifier ${name} not defined`, at)
+  }
+
+  function mustBeAVariable(entity, at) {
+    must(entity?.kind === "Variable", `Variable expected`, at)
+  }
+
+  function mustBeAProcedure(entity, at) {
+    must(entity?.kind === "Procedure", `Procedure expected`, at)
+  }
+
+  function mustBeAFunction(entity, at) {
+    must(entity?.kind === "Function", `Function expected`, at)
+  }
+
+  function mustBeWritable(variable, at) {
+    must(variable.writable, `${variable.name} is not writable`, at)
+  }
+
+  function mustHaveCorrectArgumentCount(callee, args, at) {
+    must(
+      args.length === callee.paramCount,
+      `Expected ${callee.paramCount} arg(s), found ${args.length}`,
+      at
+    )
+  }
 
   // The compiler front end analyzes the source code and produces a graph of
   // entities (defined in the core module) "rooted" at the Program entity.
@@ -69,15 +75,16 @@ export default function analyze(match) {
         context.add(id.sourceString, target)
       } else {
         // Something was found, whatever it was must be a writable variable
-        context.checkKind(target, "Variable", { at: id })
-        context.checkIsWritable(target, { at: id })
+        mustBeAVariable(target, { at: id })
+        mustBeWritable(target, { at: id })
       }
       return core.assignment(target, initializer)
     },
     Statement_call(id, exps, _semicolon) {
-      const callee = context.lookup(id, { expecting: "Procedure" })
+      const callee = context.lookup(id)
+      mustBeAProcedure(callee, { at: id })
       const args = exps.rep()
-      context.checkArguments(callee, args, { at: exps })
+      mustHaveCorrectArgumentCount(callee, args, { at: exps })
       return core.procedureCall(callee, args)
     },
     Args(_leftParen, exps, _rightParen) {
@@ -103,12 +110,16 @@ export default function analyze(match) {
     },
     Primary_id(id) {
       // In Astro, functions and procedures never stand alone, so must be a var
-      return context.lookup(id, { expecting: "Variable" })
+      const entity = context.lookup(id)
+      mustHaveBeenFound(entity, id.sourceString, { at: id })
+      mustBeAVariable(entity, { at: id })
+      return entity
     },
     Primary_call(id, exps) {
-      const callee = context.lookup(id, { expecting: "Function" })
+      const callee = context.lookup(id)
+      mustBeAFunction(callee, { at: id })
       const args = exps.rep()
-      context.checkArguments(callee, args, { at: exps })
+      mustHaveCorrectArgumentCount(callee, args, { at: exps })
       return core.functionCall(callee, args)
     },
     _terminal() {
